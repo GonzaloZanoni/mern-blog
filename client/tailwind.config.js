1b1e9ce4a9944b52129d1a6932dda62ba7d746e6
GonzaloZanoni <Gonzalozanoni3@gmail.com>
2024-05-09 20:36:29 -0300

    create the header section

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,20 @@
+/* eslint-disable no-undef */
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
+    // 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [ 
+      flowbite.plugin(),
+      ],
+};
+    
+    // plugins: [require('flowbite/plugin')],
