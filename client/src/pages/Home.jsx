2782b841f861e0aff499bbcde18cfe72ab316877
GonzaloZanoni <Gonzalozanoni3@gmail.com>
2024-05-18 02:46:43 -0300

    make the dashboard private

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,11 @@
 
 export default function Home() {
     return (
-        <div>Home</div>
+        <div>
+            <h1>Bienvenidos - Cap. José L. Ardiles</h1><br />
+            <p>
+                Somos una institucion educativa de nivel primario.
+            </p>
+        </div>
     )
 }
