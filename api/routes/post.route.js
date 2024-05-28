6857dc71ca5386f4c91bbffbcd5c76b446ac0124
GonzaloZanoni <Gonzalozanoni3@gmail.com>
2024-05-28 14:21:32 -0300

    add update post functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { create, deletepost, getposts } from '../controllers/post.controller.js';
+import { create, deletepost, getposts, updatepost } from '../controllers/post.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/create', verifyToken, create)
 router.get('/getposts', getposts)
 router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
+router.put('/updatepost/:postId/:userId', verifyToken, updatepost)
 
 export default router; 
--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -1,20 +1,46 @@
 import { Alert, Button, FileInput, Select, TextInput } from "flowbite-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import { getStorage, uploadBytesResumable, ref, getDownloadURL } from 'firebase/storage';
 import { app } from '../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useSelector } from "react-redux";
 
-export default function CreatePost() {
+export default function UpdatePost() {
     const [file, setFile] = useState(null);
     const [imageUploadProgress, setImageUploadProgress] = useState(null);
     const [imageUploadError, setImageUploadError] = useState(null);
     const [formData, setFormData] = useState({});
     const [publishError, setPublishError] = useState(null);
+    const { postId } = useParams();
+    const { currentUser } = useSelector((state) => state.user);
+
     const navigate = useNavigate();
+
+    useEffect(() => {
+        try {
+            const fetchPost = async () => {
+                const res = await fetch(`/api/post/getposts?postId=${postId}`)
+                const data = await res.json()
+                if (!res.ok) {
+                    console.log(data.message)
+                    setPublishError(data.message);
+                    return;
+                }
+                if (res.ok) {
+                    setPublishError(null);
+                    setFormData(data.posts[0]);
+                }
+            }
+            fetchPost();
+
+        } catch (error) {
+            console.log(error.message)
+        }
+    }, [postId]);
 
     // console.log(formData);
     const handleUploadImage = async () => {
@@ -45,24 +71,21 @@ export default function CreatePost() {
                         setImageUploadProgress(null);
                         setImageUploadError(null);
                         setFormData({ ...formData, image: downloadURL });
-                        // setFormData({ ...formData, image: downloadURL }); 
                     });
                 }
             );
-
-
         } catch (error) {
             setImageUploadError('Image upload failed');
             setImageUploadProgress(null);
-            console.log(error)
+            console.log(error);
         }
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await fetch('/api/post/create', {
-                method: 'POST',
+            const res = await fetch(`/api/post/updatepost/${formData._id}/${currentUser._id}`, {
+                method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json',
                 },
@@ -86,7 +109,7 @@ export default function CreatePost() {
     return (
 
         <div className="p-3 max-w-3xl mx-auto min-h-screen">
-            <h1 className="text-center text-3xl my-7 font-semibold">Create a post</h1>
+            <h1 className="text-center text-3xl my-7 font-semibold">Update a post</h1>
             <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
                 <div className="flex flex-col gap-4 sm:flex-row justify-between">
                     <TextInput
@@ -97,11 +120,13 @@ export default function CreatePost() {
                         onChange={(e) =>
                             setFormData({ ...formData, title: e.target.value })
                         }
+                        value={formData.title}
                     />
                     <Select
                         onChange={(e) =>
                             setFormData({ ...formData, category: e.target.value })
                         }
+                        value={formData.category}
                     >
                         <option value='uncategorized'>Select a category</option>
                         <option value='javascript'>Javascript</option>
@@ -114,6 +139,7 @@ export default function CreatePost() {
                         typeof="file"
                         accept='image/*'
                         onChange={(e) => setFile(e.target.files[0])}
+
                     />
                     <Button
                         type='button'
@@ -147,6 +173,7 @@ export default function CreatePost() {
                 )}
                 <ReactQuill
                     theme='snow'
+                    value={formData.content}
                     placeholder="Write something..."
                     className="h-72 mb-12"
                     required
@@ -156,7 +183,7 @@ export default function CreatePost() {
                         }
                     }
                 />
-                <Button type="submit" gradientDuoTone='greenToBlue'>Publish</Button>
+                <Button type="submit" gradientDuoTone='greenToBlue'>Update post</Button>
                 {
                     publishError && <Alert className="m-5" color='failure'>{publishError}</Alert>
                 }
